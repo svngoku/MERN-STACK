e158a12d59ede7c726c62c6896123c0d2e5dfeeb
svngoku <marymarty104@gmail.com>
2020-02-11 13:01:05 +0100

    update route for a single user

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,10 +2,20 @@ const moongose = require("mongoose");
 const User = moongose.model('user');
 
 module.exports = (app) => {
-    // GET USER 
+    // GET USERS
     app.get(`/api/user`, async (req, res) => {
         let users = await User.find();
         return res.status(200).send(users);
+    });
+
+    //  get a user by the id 
+    app.get(`/api/user/:id`, async (req, res) => {
+        const { id } = req.params;
+        let user = await User.findById(id);
+        return res.status(200).send({
+            error: false,
+            user
+        })
     });
 
     // CREATE A USER 
@@ -19,8 +29,8 @@ module.exports = (app) => {
 
     // UPDATE A USER BY ID
     app.put(`/api/user/:id`, async (req, res) => {
-        const { id } = req.params;
-        let user = User.findByIdAndUpdate(id, req.body);
+        const { ID } = req.params;
+        let user = User.findByIdAndUpdate(ID, req.body);
         return res.status(202).send({
             error: false,
             user
@@ -29,8 +39,8 @@ module.exports = (app) => {
 
     // DELETE USER BY ID
     app.delete(`/api/user/:id`, async (req, res) => {
-        const { id } = req.params;
-        let user = User.findByIdAndDelete(id, req.body);
+        const { ID } = req.params;
+        let user = User.findByIdAndDelete(ID, req.body);
         return res.status(202).send({
             error: false,
             user
