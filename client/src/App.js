3cb43d70083d3e3da6d73b50521e4d93accb74a0
svngoku <marymarty104@gmail.com>
2020-02-13 23:34:13 +0100

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,51 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Navbar from "./layouts/navbarComponent";
 import UserTable from "./tables/UserTable";
 import UserCreate from "./forms/userCreate";
-import ArticleEditor from "./layouts/editorComponent";
 
 function App() {
-
-  const userData = [
-    {  ID: 1,
-      name: "DOE",
-      username: "Jonh DOE" 
-    },
-    {  
-      ID: 2,
-      name: "Marie",
-      username: "Marie DOE" 
-    },
-  ];
-  
-  /*
-  const addUser = (user) => {
-    user.ID = users.length + 1;
-    setUser([...users, user]);
-  }
-
-  addUser={addUser}
-*/
   return (
-  <div>
-    <Navbar />
-    <div className="container">
-     <h1 className="vertical-center">CRUD App with Hooks</h1>
-      <div className="flex-row">
-        <div className="flex-large">
-          <h2>Add user</h2>
-          <UserCreate  />
-        </div>
-        <div className="flex-large">
-          <h2>View users</h2>
-          <UserTable />
+    <div>
+      <Navbar />
+      <div className="container">
+      <h1 className="vertical-center">CRUD App with Hooks</h1>
+        <div className="flex-row">
+          <div className="flex-large">
+            <h2>Add user</h2>
+            <UserCreate  />
+          </div>
+          <div className="flex-large">
+            <h2>View users</h2>
+            <UserTable />
+          </div>
         </div>
       </div>
-      <div className="flex-large">
-          <h2>Article</h2>
-          <ArticleEditor />
-      </div>
-    </div>
     </div>
   );
 }
