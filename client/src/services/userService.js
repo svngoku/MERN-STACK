a911b213229ae958f712376587236f0519fb3d4d
svngoku <marymarty104@gmail.com>
2020-02-15 01:54:05 +0100

    update user service

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,8 +1,18 @@
 import axios from "axios";
+const url = `http://localhost:4000/api/user`;
 
 export default {
     getAll: async () => {
-      let res = await axios.get(`http://localhost:4000/api/user`);
+      let res = await axios.get(url);
       return res.data || [];
+    },
+    create: async (user) => {
+        return await axios.post(url, {user})
+            .then((res) => { 
+                console.log(res);
+                console.log(res.data);
+            }).catch((err) => {
+                console.error(err);
+        })
     }
 };
