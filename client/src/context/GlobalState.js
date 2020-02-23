10bfa2779bbbda38ac2a1f5dbe4130f87524b01d
svngoku <marymarty104@gmail.com>
2020-02-23 07:59:30 +0100

    adding Context Hook

---
+++ client/src/context/GlobalState.js
@@ -0,0 +1,34 @@
+import React , {createContext , useReducer } from "react";
+import AppReducer from "./AppReducer";
+import userService from "../services/userService";
+
+const getUsers = async () => {
+  let res = await userService.getAll();
+  return res;
+};
+
+const initalState = {
+    listUser : [
+        {
+            "_id": "5e4182bcaad892d38e116be3",
+            "ID": 1,
+            "firstname": "Nelson",
+            "lastname": "Mandela",
+            "login": "Nelson Mandela"
+        }
+    ]
+};
+
+export const GlobalContext = createContext(initalState);
+
+export const GlobalProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(AppReducer, initalState);
+
+    return (
+        <GlobalContext.Provider value={{
+            users: state.users
+        }}>
+            { children }
+        </GlobalContext.Provider>
+    );
+} 
